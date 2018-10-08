f415c63e9762d9c3de78286c3d184e2d99a7213b
Four Two Eight <hiim11354428@gmail.com>
2018-10-08 16:13:03 -0400

    i made something slightly functional
    leaderboard part coming soon

---
+++ code.js
@@ -0,0 +1,7 @@
+// im doing javascript again
+function UpdateScores() {
+	document.getElementById("stxt").innerHTML = "Sunflower val. = " + document.getElementById("sun").value + ", New value = "
+	document.getElementById("htxt").innerHTML = "Hibiscus val. = " + document.getElementById("hib").value + ", New value = "
+	document.getElementById("btxt").innerHTML = "Bouquet val. = " + document.getElementById("bou").value + ", New value = "
+	document.getElementById("scores").innerHTML = document.getElementById("scores").innerHTML
+}
