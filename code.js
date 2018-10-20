d4f27a6c372fe9acfb7f79332260c5c93270899b
Four Two Eight <hiim11354428@gmail.com>
2018-10-20 09:57:29 -0400

    oh yeah forgot about this
    sfsdfsg

--- code.js
+++ code.js
@@ -147,7 +147,7 @@ userList[userList.length - 1].addItems(0, 40) // added 10/19/2018
 userList.push(new User("Milo Jacquet", [2184, 76, 13]))
 userList[userList.length - 1].addItems(7, 1)
 userList[userList.length - 1].addItems(0, 85) // added 10/12/2018 + 10/19/2018
-userList.push(new User("\u0E56\u0336\u0336\u0336\u03B6\u035C\u0361W\uD835\uDD56\uD835\uDD5A\uD835\uDD63\uD835\uDD55\uD835\uDD60\uD835\uDD67\uD835\uDD56\uD835\uDD63\uD835\uDD64\uD835\uDD56", [2237, 44, 9, 0, 1, 4]))
+userList.push(new User("\u0E56\u0336\u0336\u0336\u03B6\u035C\u0361W\uD835\uDD56\uD835\uDD5A\uD835\uDD63\uD835\uDD55\uD835\uDD60\uD835\uDD67\uD835\uDD56\uD835\uDD63\uD835\uDD64\uD835\uDD56", [2237, 34, 9, 0, 1, 4]))
 userList[userList.length - 1].addItems(20, 1)
 userList[userList.length - 1].addItems(7, 1)
 userList[userList.length - 1].addItems(0, 23) // added 10/12/2018
