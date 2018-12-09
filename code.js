8a4cc8a242866a309104896bb3ecc00df7ccef1a
Four Two Eight <hiim11354428@gmail.com>
2018-12-09 11:32:08 -0500

    leaderboard
    a

--- code.js
+++ code.js
@@ -87,10 +87,10 @@ function User(username, items) {
 }
 var userList = []
 userList.push(new User("Reidolol", [2131, 99, 13, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]))
-userList[userList.length - 1].addItems(0, 69) // lol 11/9/2018
-userList.push(new User("Milo Jacquet", [2484, 80, 13, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+userList[userList.length - 1].addItems(0, 150) // lol 11/9/2018 12/9/2018
+userList.push(new User("Milo Jacquet", [2568, 80, 13, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
 userList[userList.length - 1].addItems(0, 75) // +ed 11/9/2018
-userList.push(new User("\u0E56\u0336\u0336\u0336\u03B6\u035C\u0361W\uD835\uDD56\uD835\uDD5A\uD835\uDD63\uD835\uDD55\uD835\uDD60\uD835\uDD67\uD835\uDD56\uD835\uDD63\uD835\uDD64\uD835\uDD56", [2418, 38, 9, 0, 1, 0])) // this is weirdoverse
+userList.push(new User("\u0E56\u0336\u0336\u0336\u03B6\u035C\u0361W\uD835\uDD56\uD835\uDD5A\uD835\uDD63\uD835\uDD55\uD835\uDD60\uD835\uDD67\uD835\uDD56\uD835\uDD63\uD835\uDD64\uD835\uDD56", [2480, 38, 9, 0, 1, 0])) // this is weirdoverse
 userList[userList.length - 1].addItems(20, 1)
 userList[userList.length - 1].addItems(7, 1)
 userList[userList.length - 1].addItems(0, 23) // added 10/12/2018
@@ -105,8 +105,9 @@ userList[userList.length - 1].addItems(0, 46) // added 10/12/2018 and 11/9/2018
 userList[userList.length - 1].addItems(0, 129) // added 10/19/2018 and 11/26/2018 and 12/3/2018
 userList[userList.length - 1].addItems(0, 28) // added 10/26/2018
 userList[userList.length - 1].addItems(0, 27)  // askfghd 10/31/2018
-userList.push(new User("Denc", [191, 0, 0, 1, 0, 0, 1, 1]))
-userList.push(new User("AAAAAAAAA", [208, 5, 4, 0, 0, 0, 0, 1]))
+userList[userList.length - 1].addItems(0, 57) // ggggggggggggggggggggggggggggggggg 12/9/2018
+userList.push(new User("Denc", [191, 5, 0, 1, 0, 0, 1, 1]))
+userList.push(new User("AAAAAAAAA", [219, 5, 4, 0, 0, 0, 0, 1]))
 userList[userList.length - 1].addItems(0, 12) // added 10/26/2018
 userList[userList.length - 1].addItems(1, 5)  // added 10/26/2018
 userList[userList.length - 1].addItems(0, 31) // sfsfsfsfsfsfs 10/31/2018
